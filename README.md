b2062d45f7334a6ffe255ae4f1a4693de3631837
Joji Augustine <jojimail@gmail.com>
2020-04-26 20:17:06 +0530

    Add templates json and thumbnails to gcs

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# starter-template-images
-Template image for all anyfiddle maintained starter tempates
+# Anyfiddle Starter template images
+This repo has all the dockerfiles and configs to build docker images that are used to start a anyfiddle project
+
+## Directory structure
+- Each folder in the root is a starter template
+- Each folder must have a template.json and thumbnail.png. These will be used to show the items in template selection page
---
+++ copy-assets.sh
@@ -0,0 +1,16 @@
+templateJsons=`ls */template.json`
+
+for json in $templateJsons
+do
+  gcsJSONFilename=${json/\/template.json/.json}
+  echo "Uploading ${json} => $gcsJSONFilename"
+  gsutil cp "$json" "gs://anyfiddle-starter-templates/templatejsons/${gcsJSONFilename}"
+done
+
+templateThumbnails=`ls */thumbnail.png`
+for thumbnail in $templateThumbnails
+do
+  gcsThumbnailFilename=${thumbnail/\/thumbnail.png/.png}
+  echo "Uploading ${thumbnail} => $gcsThumbnailFilename"
+  gsutil cp "$json" "gs://anyfiddle-starter-templates/thumbnails/${gcsJSONFilename}"
+done
