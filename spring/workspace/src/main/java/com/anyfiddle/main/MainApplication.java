24de447cbe6d7d3b31e049d9c8413fe6b1971d37
Joji Augustine <jojimail@gmail.com>
2020-08-03 20:12:25 +0530

    Add spring

---
+++ spring/workspace/src/main/java/com/anyfiddle/main/MainApplication.java
@@ -0,0 +1,22 @@
+package com.anyfiddle.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+
+}
