063cb7ea139146b71776a38225ba5cc4c8f9a92f
bosam <supertop>
2020-09-17 07:40:32 -0700

    Added config/routes.yaml to openFiles and commented route annotation inside the controller

--- symfony/workspace/src/Controller/DefaultController.php
+++ symfony/workspace/src/Controller/DefaultController.php
@@ -2,14 +2,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
-    /**
-     * @Route("/")
-     */
     public function index() : Response
     {
         return $this->render('default.html.twig');
